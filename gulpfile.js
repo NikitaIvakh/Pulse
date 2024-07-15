184fb15ea1b5e3486daec53c598b27197b7e1f1d
Nikita Ivakh <nikita.ivakh7@gmail.com>
2024-07-15 16:17:57 +0300

    add css files

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const sass = require('gulp-sass')(require('sass'))
 const cleanCSS = require('gulp-clean-css')
 const rename = require('gulp-rename')
 
-gulp.task('server', () => {
+gulp.task('server', function () {
 	browser.init({
 		server: {
 			baseDir: 'src',
@@ -17,14 +17,14 @@ gulp.task('server', () => {
 	gulp.watch('src/*.html').on('change', browser.reload)
 })
 
-gulp.task('style', () => {
+gulp.task('styles', function () {
 	return gulp
 		.src('src/sass/**/*.+(scss|sass)')
-		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
 		.pipe(rename({ suffix: '.min', prefix: '' }))
 		.pipe(cleanCSS({ compatibility: 'ie8' }))
-		.pipe(gulp.dest('dist'))
-		.pipe(browser.stream)
+		.pipe(gulp.dest('src/css'))
+		.pipe(browser.stream())
 })
 
-gulp.task('default', gulp.parallel('server', 'style'))
+gulp.task('default', gulp.parallel('server', 'styles'))
