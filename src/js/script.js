6680d9b5fc69ea7647518281e6bd1a90b4674ef6
Nikita Ivakh <nikita.ivakh7@gmail.com>
2024-07-20 20:20:09 +0300

    add modal widnows

--- src/js/script.js
+++ src/js/script.js
@@ -78,4 +78,20 @@ $(document).ready(function () {
 
 	toggleSlide('.catalog-item__back')
 	toggleSlide('.catalog-item__link')
+
+	// Modal
+	$('[data-modal=consulting]').on('click', function () {
+		$('.overlay, #consulting').fadeIn('slow')
+	})
+
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consulting, #order, #thanks').fadeOut('slow')
+	})
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+			$('.overlay, #order').fadeIn('slow')
+		})
+	})
 })
