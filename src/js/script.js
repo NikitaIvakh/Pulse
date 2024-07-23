b6cf181b8af15e2d11d802ba0fb616dee19b9ac0
Nikita Ivakh <nikita.ivakh7@gmail.com>
2024-07-23 19:58:13 +0300

    add smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -156,4 +156,31 @@ $(document).ready(function () {
 				console.error('Error:', textStatus, errorThrown)
 			})
 	})
+
+	// pageup
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn()
+		} else {
+			$('.pageup').fadeOut()
+		}
+	})
+
+	// Smooth scrolling
+	$("a[href^='#']").on('click', function (event) {
+		if (this.hash !== '') {
+			event.preventDefault()
+			var hash = this.hash
+
+			$('html, body').animate(
+				{
+					scrollTop: $(hash).offset().top,
+				},
+				0,
+				function () {
+					window.location.hash = hash
+				}
+			)
+		}
+	})
 })
